826e56e430c875a087bb84fd7aabb0fa93ac701b
avrybrdly93 <cbra067@lasierra.edu>
2019-02-06 11:46:44 -0800

    Revert "Charles"

--- public/js/signup.js
+++ public/js/signup.js
@@ -4,16 +4,16 @@ $(document).ready(function () {
   let userPhoneNum = '';
   let password = '';
   let selectedState;
-
+  
   $(".phoneInput").keyup(function () {
     if (this.value.length == this.maxLength) {
-      $(this).next('.phoneInput').focus();
-      phoneVals += $(this)[0].value;
-    }
-    if (phoneVals.length === 10) {
+        $(this).next('.phoneInput').focus();
+        phoneVals += $(this)[0].value;
+  }
+    if(phoneVals.length === 10) {
       userPhoneNum = phoneVals;
     }
-  });
+  });  
 
   function checkPasswordMatch() {
     password = $("#passwordInput").val();
@@ -27,57 +27,57 @@ $(document).ready(function () {
       return true;
     }
 
-  }
-  $("#passwordInputConfirm").keyup(checkPasswordMatch);
+}
+   $("#passwordInputConfirm").keyup(checkPasswordMatch);
 
 
-  $("select.uk-select").change(function () {
+$("select.uk-select").change(function(){
     selectedState = $(this).children("option:selected").val();
-  });
+});
 
-  $("#submitBtn").on("click", function (event) {
-    event.preventDefault();
+$("#submitBtn").on("click", function (event) {
+  event.preventDefault();
 
-    if (checkPasswordMatch()) {
-      var newUsr = {
-        "first_name": $("#fNameInput").val().trim(),
-        "username": $("#userName").val().trim(),
-        "last_name": $("#lNameInput").val().trim(),
-        "address": $("#addressInput").val().trim(),
-        "city": $("#cityInput").val().trim(),
-        "state": selectedState,
-        "zip": $("#zipInput").val().trim(),
-        "email": $("#emailInput").val().trim(),
-        "phone": userPhoneNum,
-        "account_key": password
-      };
-      console.log(newUsr);
-      $.post("/signup", newUsr, function (data, status, xhr) {
-        console.log(data);
-        console.log(status);
-        console.log(xhr);
-
-        switch (xhr.status) {
-          case 200: {
-            window.location.href = "/dashboard";
-            break;
-          }
-          case 401: {
-            window.location.href = "/signup";
-            break;
-          }
-          case 404: {
-            window.location.href = "*";
-            break;
-          }
-          case 500: {
-            alert("Refresh Page!");
-            break;
-          }
+  if(checkPasswordMatch() && password.length > 7) {
+    var newUsr = {
+      "username": $("#userName").val().trim(),
+      "first_name": $("#fNameInput").val().trim(),
+      "last_name": $("#lNameInput").val().trim(),
+      "address": $("#addressInput").val().trim(),
+      "city": $("#cityInput").val().trim(),
+      "state": selectedState,
+      "zip": $("#zipInput").val().trim(),
+      "email": $("#emailInput").val().trim(),
+      "phone": userPhoneNum,
+      "account_key": password
+    };
+    //console.log(newUsr);
+    $.post("/signup", newUsr, function (data, status, xhr) {
+      console.log(data);
+      console.log(status);
+      console.log(xhr);
+      
+      switch (xhr.status) {
+        case 200: {
+          window.location.href = "/dashboard";
+          break;
         }
-      });
-    } else {
-      alert("passwords do not match");
-    }
-  });
+        case 401: {
+          window.location.href = "/signup";
+          break;
+        }
+        case 404: {
+          window.location.href = "*";
+          break;
+        }
+        case 500: {
+          alert("Refresh Page!");
+          break;
+        }
+      }  
+    });
+  } else {
+    alert("passwords do not match");
+  }
+});
 });

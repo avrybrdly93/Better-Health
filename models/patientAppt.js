d466d6cdf3aa58498645ce31d2d20b978f539a5c
javelaptop <jventura1097@gmail.com>
2019-02-06 20:08:39 -0800

    two test files, authentication to routes

--- models/patientAppt.js
+++ models/patientAppt.js
@@ -20,6 +20,13 @@ module.exports = function(sequelize,DataTypes){
             validate: {
                 len: [1,30]
             }
+        },
+        appt_reason:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate:{
+                len: [1]
+            }
         }
     });
 

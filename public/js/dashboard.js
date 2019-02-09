edd33dc3ca6f45e6dd104b5451dabc88e6783f6c
Kfill <fillpotk@gmail.com>
2019-02-08 21:15:10 -0800

    some very basic styling for the appointment modal info

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,52 +1,53 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var docFName;
     var docLName;
     var docID;
 
-    $("#bookBtn").on("click",function(){
+    $("#bookBtn").on("click", function () {
         $.ajax({
             method: "GET",
             url: "/api/staff"
-        }).then(function(result){
+        }).then(function (result) {
             console.log(result);
             $("#modalBody").empty();
             $("#modalTitle").text("Choose Appointment Staff");
 
-            var someSpace=$("<div>");
+            var someSpace = $("<div>");
 
-            if(result.length>0){
-                for(var i=0;i<result.length;i++){
-                    var someDiv=$("<div>");
-                    $(someDiv).append("<p>"+result[i].last_name+", "+result[i].first_name+" - "+result[i].title+" - "+result[i].specialization);
+            if (result.length > 0) {
+                for (var i = 0; i < result.length; i++) {
+                    var someDiv = $("<div>");
+                    $(someDiv).append("<p>" + result[i].last_name + ", " + result[i].first_name + " - " + result[i].title + " - " + result[i].specialization);
                     var newBtn = $("<button>Book Me</button>");
-    
+
                     $(newBtn).attr({
                         "data-id": result[i].uuid,
                         "data-fName": result[i].first_name,
                         "data-lName": result[i].last_name,
                         "class": "btn"
                     });
-    
+
                     $(newBtn).addClass("staffBookMe");
-    
+
                     $(someDiv).append(newBtn);
-    
+
                     $(someSpace).append(someDiv);
-    
+
                 }
-                
+
                 $("#modalBody").append(someSpace);
-            }
-            else{
+            } else {
                 $("#modalBody").append("No Doctors Available Right Now. Check Again Soon.");
             }
+
+            $("modalBody").empty();
         });
     });
 
-    $('body').on('click', '.staffBookMe', function() {
-        docFName=$(this).attr("data-fName");
-        docLName=$(this).attr("data-lName");
-        docID=$(this).attr("data-id");
+    $('body').on('click', '.staffBookMe', function () {
+        docFName = $(this).attr("data-fName");
+        docLName = $(this).attr("data-lName");
+        docID = $(this).attr("data-id");
 
         console.log(docID);
         console.log(docFName);
@@ -58,24 +59,26 @@ $(document).ready(function(){
 
         $("#modalBody").append("<p>Fill out the form below to book your next appointment.</p>");
 
-        var newForm=$("<form>").addClass("uk-grid-small");
-        
-        $(newForm).append("Date: <input type='text' id='dateField'>   ");
+        var newForm = $("<form>").addClass("uk-grid-small");
+
+        // This is for the date-picker
+        $(newForm).append("Date: <input type='text' id='dateField'>  ");
         $(newForm).append("Time: <input type='text' id='timeField'><br><br>");
         $(newForm).append("Reason: <input type='text' id='reasonField'><br>");
         $(newForm).append("<br><button id='bookFormSubmit'>Submit</button>");
 
         $("#modalBody").append(newForm);
+
     });
 
-    $('body').on('click', '#bookFormSubmit', function() {
+    $('body').on('click', '#bookFormSubmit', function () {
         event.preventDefault();
-        
+
         var dateField = $("#dateField").val();
         var timeField = $("#timeField").val();
         var apptReason = $("#reasonField").val();
 
-        var newAppt={
+        var newAppt = {
             date: $("#dateField").val(),
             time: $("#timeField").val(),
             appt_reason: $("#reasonField").val(),
@@ -85,12 +88,12 @@ $(document).ready(function(){
 
         console.log(newAppt);
 
-        $.ajax("/api/appointment/"+docID,{
+        $.ajax("/api/appointment/" + docID, {
             type: "POST",
             data: newAppt
-        }).then(function(result){
+        }).then(function (result) {
             console.log(result);
-            
+
         });
 
         $("#modalBody").empty();
@@ -101,22 +104,23 @@ $(document).ready(function(){
         $("#modalBody").append("<h2>Time:</h2><h3>" + timeField + "</h3>");
         $("#modalBody").append("<h2>Reason:</h2><h3>" + apptReason + "</h3>");
 
-        location.reload();
-    }); 
+        
+    });
 
-// Card for Appointment
+    // Card for Appointment
 
-$.ajax({
-    method: "GET",
-    url: "/api/appointments"
-}).then(function(result){
-    console.log(result);
-    if(result.length > 0){
-        $("#book-card-title").text(`Next Appointment`)
-        $("#book-card-body").text(`${result[result.length-1].appt_reason} with ${result[result.length-1].staff_fName} ${result[result.length-1].staff_lName} at ${result[result.length-1].time} on ${result[0].date}`)
-    }
-    
-});
+    $.ajax({
+        method: "GET",
+        url: "/api/appointments"
+    }).then(function (result) {
+        console.log(result);
+        if (result.length > 0) {
+            $("#icon-div").append("uk-icon", "icon: bolt; ratio: 1.5")
+            $("#book-card-title").text(` Next Appointment`)
+            $("#book-card-body").text(`${result[result.length-1].appt_reason} with ${result[result.length-1].staff_fName} ${result[result.length-1].staff_lName} at ${result[result.length-1].time} on ${result[0].date}`)
+        }
+
+    });
 
 
 
